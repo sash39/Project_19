9116c2f38cff2850a5344cf0135e2b148817bccc
sash39 <aleksandr.egorkin@gmail.com>
2023-02-18 20:45:48 +0200

    Поправил login, убрал не рабочие кнопки с Home

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import UserRegistrationView, UserLoginView, UserLogoutView
+from users.views import UserRegistrationView, UserLoginView, UserLogoutView, RedirectView
 
 
 urlpatterns = [
     path('registration', UserRegistrationView.as_view(), name='registration'),
     path('login', UserLoginView.as_view(), name='login'),
     path('logout', UserLogoutView.as_view(), name='logout'),
+    path('index', RedirectView.as_view(), name = 'index' )
 ]
